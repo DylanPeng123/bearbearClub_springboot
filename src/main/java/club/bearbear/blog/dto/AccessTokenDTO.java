e1b3dfdb26e2265dce4871c8d772146bc7d1a3a9
Dylan <pengdahai216@126.com>
2021-03-11 05:44:22 +0800

    移出lombok

--- src/main/java/club/bearbear/blog/dto/AccessTokenDTO.java
+++ src/main/java/club/bearbear/blog/dto/AccessTokenDTO.java
@@ -1,6 +1,6 @@
 package club.bearbear.blog.dto;
 
-import lombok.Data;
+import java.util.StringJoiner;
 
 /**
  * ClassName AccessTokenDTO
@@ -9,7 +9,7 @@ import lombok.Data;
  * @description github认证 dto 数据传输模型
  * @createDate 2020-03-04 16:45
  */
-@Data
+
 public class AccessTokenDTO {
 
     private String client_id;
@@ -18,4 +18,54 @@ public class AccessTokenDTO {
     private String redirect_uri;
     private String state;
 
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_secret() {
+        return client_secret;
+    }
+
+    public void setClient_secret(String client_secret) {
+        this.client_secret = client_secret;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getRedirect_uri() {
+        return redirect_uri;
+    }
+
+    public void setRedirect_uri(String redirect_uri) {
+        this.redirect_uri = redirect_uri;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", AccessTokenDTO.class.getSimpleName() + "[", "]")
+                .add("client_id='" + client_id + "'")
+                .add("client_secret='" + client_secret + "'")
+                .add("code='" + code + "'")
+                .add("redirect_uri='" + redirect_uri + "'")
+                .add("state='" + state + "'")
+                .toString();
+    }
 }
--- src/main/java/club/bearbear/blog/dto/GithubUser.java
+++ src/main/java/club/bearbear/blog/dto/GithubUser.java
@@ -1,6 +1,6 @@
 package club.bearbear.blog.dto;
 
-import lombok.Data;
+import java.util.StringJoiner;
 
 /**
  * ClassName GithubUser
@@ -9,7 +9,7 @@ import lombok.Data;
  * @description Github授权的用户信息
  * @createDate 2020-03-04 18:57
  */
-@Data
+
 public class GithubUser {
 
     private String id;
@@ -17,4 +17,45 @@ public class GithubUser {
     private String bio;
     private String email;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", GithubUser.class.getSimpleName() + "[", "]")
+                .add("id='" + id + "'")
+                .add("name='" + name + "'")
+                .add("bio='" + bio + "'")
+                .add("email='" + email + "'")
+                .toString();
+    }
 }
